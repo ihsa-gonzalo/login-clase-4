060e6a9813fe1c072a914191c7ba174e515bce4b
S684256 <rosana.cespedes@evertecinc.com>
2022-10-15 12:05:27 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/ui/screens/home_screen.dart';
+import 'package:flutter_login/ui/screens/login_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login',
+      initialRoute: 'login',
+      routes: {
+        'login': (_) => LoginScreen(),
+        'home': (_) => HomeScreen(),
+      },
+      theme:
+          ThemeData.light().copyWith(scaffoldBackgroundColor: Colors.grey[300]),
+    );
+  }
+}
---
+++ lib/ui/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purpleAccent,
+      body: Center(
+        child: Container(
+          child: 
+          Text('En proceso.....'),
+        ),
+      ),
+    );
+  }
+}
